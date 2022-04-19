dd700b49aac07519d70ad0d91e95328c62612c51
Derek <derek.hammer@discordapp.com>
2022-04-19 09:35:04 -0600

    create a new session with discord-0 as the name

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install -y silversearcher-ag
 sudo npm install -g typescript typescript-language-server
 sudo npm install -g pyright
-tmux new-session -d
+tmux new -d -s discord-0
 /home/discord/tmux-resurrect/scripts/restore.sh
