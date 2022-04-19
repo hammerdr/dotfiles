9fc539c648735d77947aefabebc73083d97fb6f7
Derek <derek.hammer@discordapp.com>
2022-04-19 09:29:20 -0600

    create a tmux session before restoring it

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt-get update
 sudo apt-get install -y silversearcher-ag
 sudo npm install -g typescript typescript-language-server
 sudo npm install -g pyright
+tmux new-session -d
 /home/discord/tmux-resurrect/scripts/restore.sh
