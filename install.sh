e42b874a58395b1a7fef83e25a34d8ffacfb0916
Derek <derek.hammer@discordapp.com>
2022-04-20 10:06:50 -0600

    Skip tmux-resurrect

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ sudo apt-get update
 sudo apt-get install -y silversearcher-ag
 sudo npm install -g typescript typescript-language-server
 sudo npm install -g pyright
-tmux new -d -s discord-0
-/home/discord/tmux-resurrect/scripts/restore.sh
+# Temporarily skipping because restore might be borking things
+# tmux new -d -s discord-0
+# /home/discord/tmux-resurrect/scripts/restore.sh
