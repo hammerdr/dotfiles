c5debff10ea3740b5966e189f34d3c592da5cc5f
Derek Hammer <derek.hammer@discordapp.com>
2023-05-15 14:57:40 +0000

    fix a couple of bugs

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y silversearcher-ag neovim
 sudo npm install -g typescript typescript-language-server
 sudo npm install -g pyright
-sudo pip3 install ruff-lsp
+sudo pip install --upgrade pip
+sudo pip install ruff-lsp
 # Temporarily skipping because restore might be borking things
 # tmux new -d -s discord-0
 # /home/discord/tmux-resurrect/scripts/restore.sh
