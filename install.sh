3e3345d355241191a62a00aab9e3595b16da69b5
Derek Hammer <derek.hammer@discordapp.com>
2023-06-28 23:34:58 +0000

    update to pip3

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 sudo apt-get install -y silversearcher-ag neovim
 sudo npm install -g typescript typescript-language-server
 sudo npm install -g pyright
-sudo pip install --upgrade pip
-sudo pip install python-lsp-server
-sudo pip install pylsp-mypy
-sudo pip install python-lsp-black
-sudo pip install python-lsp-ruff
+sudo pip3 install --upgrade pip
+sudo pip3 install python-lsp-server
+sudo pip3 install pylsp-mypy
+sudo pip3 install python-lsp-black
+sudo pip3 install python-lsp-ruff
 # Temporarily skipping because restore might be borking things
 # tmux new -d -s discord-0
 # /home/discord/tmux-resurrect/scripts/restore.sh
